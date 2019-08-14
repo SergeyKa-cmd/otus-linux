e9d01fff702286f303441b59f7ac7409cd8445ed
Taras Zaviryukha <taraszav@gmail.com>
2019-08-14 18:33:06 +0300

    Add HW4

---
+++ HW4/README.md
@@ -0,0 +1,30 @@
+
+# OTUS Linux admin course
+
+## BASH 
+
+### Nginx log analyser
+
+Wrote some simple script for analysing nginx log. You need to set files path and logrotate variable ON or OFF.
+
+[Script](nginx_la.sh)
+
+[Crontab](crontab)
+
+```
+
+```
+
+## Useful links
+
+https://www.tldp.org/LDP/Bash-Beginners-Guide/html/
+
+https://www.tldp.org/LDP/abs/html/
+
+https://www.opennet.ru/docs/RUS/bash_scripting_guide/
+
+https://linuxconfig.org/learning-linux-commands-sed
+
+https://www.opennet.ru/docs/RUS/awk/
+
+https://help.ubuntu.ru/wiki/bash
---
+++ HW4/nginx_la.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Simple Nginx log analyser
+
+#######################
+# Variables
+
+lockfile=/tmp/nla.lock
+logfile=./nginx/access.log.test
+errorfile=./nginx/error.log.1
+resultfile=/tmp/resultfile.txt
+usermail=root@localhost
+shiftfile=/tmp/shiftfile
+typeset -i shift=0
+typeset -i filelong
+hostname=`uname -n`
+
+# you can set logrotate ON or OFF. if OFF then script try to calc shift in log file after last start
+logrotate="OFF"
+
+#######################
+# Functions
+
+clean_up() {
+	rm -f $lockfile
+    rm -f $resultfile
+	exit 255
+}
+
+cat_logfile() {
+    tail -n +$shift $logfile
+}
+
+sort_top() {
+    sort \
+    | uniq -c \
+    | sort -nr \
+    | head -10
+}
+
+get_request_ips(){
+    echo ""
+    echo "Top 10 Request IP's:"
+    echo "===================="
+    cat_logfile \
+    | awk '{print $1}' \
+    | sort_top
+    echo ""
+}
+
+get_request_pages(){
+    echo "Top 10 Request Pages:"
+    echo "====================="
+    cat_logfile \
+    | egrep "GET.*http" \
+    |  awk '{print $11}' \
+    | sort_top
+    echo ""
+}
+
+get_request_retcode(){
+    echo "Top 10: Page Return Codes:"
+    echo "=========================="
+    cat_logfile\
+    | awk '{print $9}' \
+    | sort_top
+    echo ""
+}
+
+get_errors(){
+    echo "All errors:"
+    echo "==========="
+    cat $errorfile \
+    | egrep "\[error\]"
+    echo ""
+}
+
+get_shift(){
+if [ -f $shiftfile ]; then
+    shift=`cat $shiftfile`
+    filelong=`wc -l $logfile | awk '{print $1}'`
+    if [ $shift -gt $filelong ]; then
+        echo "Warning: Something goes wrong, starting from begin of log."
+        echo
+        shift="0"
+    else
+        shift+=1
+        wc -l $logfile | awk '{print $1}'> $shiftfile
+    fi
+else
+    shift="0"
+    wc -l $logfile | awk '{print $1}'> $shiftfile
+
+fi
+}
+
+
+#######################
+# Main
+
+# set trap
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null;
+then
+    trap clean_up SIGHUP SIGINT SIGTERM
+else
+    echo "Failed to acquire lockfile: $lockfile."
+    echo "Held by $(cat $lockfile)"
+    exit 255
+fi
+
+# send all output ot file
+exec &> $resultfile
+
+# if OFF then script try to calc shift 
+if [ $logrotate == "OFF" ]; then
+    get_shift
+fi
+
+echo "**************************************"
+echo "*** Last hour nginx log statistics ***"
+echo "**************************************"
+echo "Hostname: $hostname"
+echo "---------------------------------------"
+# get statistics
+get_request_ips
+get_request_pages
+get_request_retcode
+get_errors
+
+echo "Log parsed"
+
+# send mail to recipient
+sendmail $usermail < $resultfile
+
+# delete temp files
+clean_up
